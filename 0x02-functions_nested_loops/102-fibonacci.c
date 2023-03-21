d21330e9b203cc46e0d5333b9069d5cfa1c0689b
Bill958 <kabogobill4@gmail.com>
2023-03-21 23:41:23 +0300

    The World looks like a multiplication-table, or a mathematical equation, which, turn it how you will, balances itself

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,29 +1,28 @@
 #include <stdio.h>
+
 /**
- * main - Prints the first 52 fibonacci numbers
- * * Return: Nothing!
+ * main - Prints first 50 Fibonacci numbers, starting with 1 and 2,
+ *        separated by a comma followed by a space.
+ * Return: Always 0.
  */
 int main(void)
 {
-	int i = 0;
-	long j = 1, k = 2;
+	int count;
+	unsigned long fib1 = 0, fib2 = 1, sum;
 
-	while (i < 50)
+	for (count = 0; count < 50; count++)
 	{
-		if (i == 0)
-			printf("%ld", j);
-		else if (i == 1)
-			printf(", %ld", k);
-		else
-		{
-			k += j;
-			j = k - j;
-			printf(", %ld", k);
-		}
+		sum = fib1 + fib2;
+		printf("%lu", sum);
 
-		++i;
+		fib1 = fib2;
+		fib2 = sum;
+
+		if (count == 49)
+			printf("\n");
+		else
+			printf(", ");
 	}
 
-	printf("\n");
 	return (0);
 }
