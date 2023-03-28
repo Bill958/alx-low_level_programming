8d5b89f451e14d9450cfdd243e2c65dac999cb64
bill958 <kabogobill4@gmail.com>
2023-03-28 06:46:33 +0300

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string, in reverse,then a new line
+ * @s: pointer to be prited
+ */
+void print_rev(char *s)
+{
+	int i, j, len;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	len = i;
+
+	for (j = len - 1; j >= 0; j--)
+	{
+		_putchar(s[j]);
+	}
+
+	_putchar('\n');
+}
